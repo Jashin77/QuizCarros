768a83b56776c79272b6e8eb8a6a391215b771d1
Braian Dos Santos Da Silva <122707041+Jashin77@users.noreply.github.com>
2025-07-23 12:00:05 -0300

    update

---
+++ Migrations/20250723145630_CorrigirTextoTurboCompressor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizCarros.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigirTextoTurboCompressor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+               "UPDATE perguntas " +
+               "SET Alternativa2 = 'Aumentar a potência, colocando mais ar dentro do motor' " +
+               "WHERE Enunciado = 'Qual a função de um turbo compressor em um automovél?'");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE perguntas " +
+                "SET Alternativa2 = 'Aumentar a potência,colocando mair ar dentro do motor' " +
+                "WHERE Enunciado = 'Qual a função de um turbo compressor em um automovél?'");
+        }
+    }
+}
